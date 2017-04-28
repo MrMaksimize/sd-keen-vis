878f86e82efa642cbafcc4778a678cc2e8ebb578
Maksim Pecherskiy <max@maksimize.com>
2017-04-28 14:38:06 -0700

    Connecting to keen for query.

--- sd-keen-vis.es6.js
+++ sd-keen-vis.es6.js
@@ -1,19 +1,83 @@
 (function() {
   Polymer({
 
-    is: 'sd-keen-vis', 
+    is: 'sd-keen-vis',
 
     properties: {
-      /**
-      * This property keeps track of the number of clicks.
-      *
-      * @property counterValue
-      */
-      counterValue: {
-        type: Number,
-        value: 0,
-        notify: true
-      }
+        /**
+        * This property keeps track of the number of clicks.
+        *
+        * @property counterValue
+        */
+        counterValue: {
+            type: Number,
+            value: 0,
+            notify: true
+        },
+        project: {
+            type: String
+        },
+        readKey: {
+            type: String
+        },
+        collection: {
+            type: String
+        },
+        analysisType: {
+            type: String
+        },
+        timeFrame: {
+            type: String
+        },
+        interval: {
+            type: String
+        }
+    },
+    observers: [
+        //TODO -- these three should be an object
+        '_initClient(project, key)',
+        '_updateQuery(collection, analysisType)'
+    ],
+    created: function() {
+        this.config = {};
+    },
+    ready: function() {
+        Keen.ready(function() {
+            console.log('Keen ready.');
+        });
+    },
+    _initClient: function() {
+        console.log('initClient');
+        this.client = new Keen({
+            projectId: this.project,
+            readKey: this.readKey
+        });
+    },
+    _updateQuery: function() {
+        this.query = new Keen.Query(this.analysisType, {
+            event_collection: this.collection,
+            timeframe: this.timeFrame,
+            //targetProperty: this.property,
+            interval: this.interval
+            //groupBy: this.group,
+            //filters: this.filters
+        });
+        this._runQuery();
+    },
+    _runQuery: function() {
+        if (!this.client) {
+            this._initClient();
+        }
+        this.client.run(this.query, function(err, res){
+            if (err) {
+                // there was an error!
+                console.log('Stupid error.')
+                console.log(err);
+            }
+            else {
+                console.log(res.result)
+            }
+        });
     },
 
     /**
