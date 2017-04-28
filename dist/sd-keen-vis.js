705261f2f82a929cf4b30c52f9225f12ac085538
Maksim Pecherskiy <max@maksimize.com>
2017-04-28 13:48:47 -0700

    Initial.

---
+++ dist/sd-keen-vis.js
@@ -0,0 +1,2 @@
+'use strict';(function(){Polymer({is:'sd-keen-vis',properties:{counterValue:{type:Number,value:0,notify:true}},handleClick:function handleClick(event,detail,sender){this.increment()},increment:function increment(){this.counterValue++}})})();
+//# sourceMappingURL=sd-keen-vis.js.map
